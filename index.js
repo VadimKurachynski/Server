16ff2f833956cb88c2b0ddd649484ee22c11d318
VadimKurachynski <kurachynski@gmail.com>
2022-12-27 19:30:41 +0300

    posl

--- index.js
+++ index.js
@@ -15,12 +15,13 @@ app.get('/', (request, response) => {
     response.json({info: 'Node.js, Express, and Postgres API'})
 })
 
-app.get('/users', db.getUsers)
-app.get('/users/:id', db.getUserById)
-app.post('/users', db.createUser)
-app.put('/users/:id', db.updateUser)
-app.delete('/users/:id', db.deleteUser)
-app.get('/nom', db.getQuestion)
+// app.get('/users', db.getUsers)
+// app.get('/users/:id', db.getUserById)
+// app.post('/users', db.createUser)
+// app.put('/users/:id', db.updateUser)
+// app.delete('/users/:id', db.deleteUser)
+// app.get('/nom', db.getQuestion)
+
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
