c1a97ab9fee9509ae3b6a01a0c48de7b449fdacf
VadimKurachynski <kurachynski@gmail.com>
2022-12-29 17:18:04 +0300

    23ser

--- serversession.js
+++ serversession.js
@@ -1,4 +1,8 @@
 const express =require('express')
 const app =express();
 
-app.get("/",)
+app.get("/",(req,res)=>{
+    res.send("Hello Session");
+});
+
+app.listen(5000, console.log("Server Running on port=5000"));
