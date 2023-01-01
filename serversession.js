b3c06ebcd30807a352371544df0d0ceecabe4e24
VadimKurachynski <kurachynski@gmail.com>
2023-01-01 19:06:46 +0300

    serv

--- serversession.js
+++ serversession.js
@@ -1,42 +1,37 @@
-const express =require('express');
-const session=require('express-session');
-const MongoDBSession=require('connect-mongodb-session')(session);
-const mongoose=require("mongoose");
-const app =express();
+const express = require('express');
+const session = require('express-session');
+const MongoDBSession = require('connect-mongodb-session')(session);
+const mongoose = require("mongoose");
+const app = express();
 const port = 5000;
-const mongoURI='mongodb://localhost:27017/sessions'
+const mongoURI = 'mongodb://localhost:27017/sessions'
 
 mongoose.set("strictQuery", false);
-mongoose.connect(mongoURI,{
+mongoose.connect(mongoURI, {
     useNewUrlParser: true,
-   // useCreateIndex: true,
+    // useCreateIndex: true,
     useUnifiedTopology: true
-})
-    .then((res)=>{
+}).then((res) => {
     console.log("MongoDB Connected");
 });
-
-const store=new MongoDBSession({
-    uri:mongoURI,
+const store = new MongoDBSession({
+    uri: mongoURI,
     collection: 'mySessions',
 })
-
 app.use(session({
-    secret:'key this key',
-    resave:false,
-    saveUninitialized:false,
-    store:store,
+    secret: 'key this key',
+    resave: false,
+    saveUninitialized: false,
+    store: store,
 }))
-
-app.get("/",(req,res)=>{
-     req.session.isAuth=true;
-     req.session.is=false;
-     req.session.hello=67;
+app.get("/", (req, res) => {
+    req.session.isAuth = true;
+    req.session.is = false;
+    req.session.hello = 67;
     console.log(req.session);
     console.log(req.session.id);
     res.send("Hello Session");
 });
-
 app.listen(port, () => {
     console.log(`App running on port ${port}.`)
 })
