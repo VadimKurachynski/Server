5b960a1f0881e98cd35f1caefb2252b25da21aad
VadimKurachynski <kurachynski@gmail.com>
2023-02-05 00:05:41 +0300

    6

--- serversession.js
+++ serversession.js
@@ -1,37 +1,37 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const session = require("express-session");
-const MongoDBStore = require("connect-mongodb-session")(session);
+// const MongoDBStore = require("connect-mongodb-session")(session);
 const config = require("config");
 const appController = require("./controllers/appController");
 const cors=require("cors");
 const isAuth = require("./middleware/is-auth");
 // const connectDB = require("./config/dbMn");
 const dbPg = require('./controllers/queries');
-const mongoURI = config.get("mongoURI");
+// const mongoURI = config.get("mongoURI");
 const app = express();
 const port = 5001;
 console.log("тут1")
 // connectDB();
 console.log("тут2")
-const store = new MongoDBStore({
-    uri: mongoURI,
-    collection: "mySessions",
-});
+// const store = new MongoDBStore({
+//     uri: mongoURI,
+//     collection: "mySessions",
+// });
 app.use(cors({credentials: true, origin: 'http://localhost:3001'}));
 // app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-    session({
-        secret: "secret",
-        resave: false,
-        saveUninitialized: false,
-        store: store,
-    })
-);
+// app.use(
+//     session({
+//         secret: "secret",
+//         resave: false,
+//         saveUninitialized: false,
+//         store: store,
+//     })
+// );
 
 //------------------------------------------------------------
 app.post("/login", appController.login_post);
