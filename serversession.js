c53121d36564268e7baf7252140d0f1e27672162
VadimKurachynski <kurachynski@gmail.com>
2023-02-05 10:33:51 +0300

    pr

--- serversession.js
+++ serversession.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require('body-parser');
 const session = require("express-session");
-// const MongoDBStore = require("connect-mongodb-session")(session);
+const MongoDBStore = require("connect-mongodb-session")(session);
 const config = require("config");
 const appController = require("./controllers/appController");
 const cors=require("cors");
@@ -12,26 +12,26 @@ const mongoURI = config.get("mongoURI");
 const app = express();
 const port = 5001;
 console.log("тут1")
-connectDB();
+ connectDB();
 console.log("тут2")
-// const store = new MongoDBStore({
-//     uri: mongoURI,
-//     collection: "mySessions",
-// });
+const store = new MongoDBStore({
+    uri: mongoURI,
+    collection: "mySessions",
+});
 app.use(cors({credentials: true, origin: 'http://localhost:3001'}));
 // app.set("view engine", "ejs");
 
 app.use(bodyParser.json());
 app.use(express.urlencoded({ extended: true }));
 
-// app.use(
-//     session({
-//         secret: "secret",
-//         resave: false,
-//         saveUninitialized: false,
-//         store: store,
-//     })
-// );
+app.use(
+    session({
+        secret: "secret",
+        resave: false,
+        saveUninitialized: false,
+        store: store,
+    })
+);
 //------------------------------------------------------------
 app.post("/login", appController.login_post);
 app.post("/logout", appController.logout_post);
