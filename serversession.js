70243f6e10e0496355f7863e461943e495a42f7f
VadimKurachynski <kurachynski@gmail.com>
2023-02-06 23:39:32 +0300

    refend

--- serversession.js
+++ serversession.js
@@ -4,17 +4,16 @@ const session = require('express-session');
 const MongoDBStore = require("connect-mongodb-session")(session);
 const config = require('config');
 const appController = require('./controllers/appController');
-const cors=require("cors");
+const cors = require("cors");
 const isAuth = require("./middleware/is-auth");
 const dbPg = require('./controllers/queries');
 const mongoose = require("mongoose");
-const configMy=require('./config/config')
 mongoose.set('strictQuery', false);
 const app = express();
+const configMy = require('./config/config')
 const port = configMy.Port;
-const URI=configMy.URImy;
-mongoURI="mongodb://127.0.0.1:27017/sessions";
- console.log(process.env);
+const URI = configMy.URImy;
+mongoURI = "mongodb://127.0.0.1:27017/sessions";
 const connectDB = async () => {
     try {
         await mongoose.connect(mongoURI, {
@@ -27,21 +26,14 @@ const connectDB = async () => {
         process.exit(1);
     }
 };
-
-
 const store = new MongoDBStore({
     uri: mongoURI,
     collection: "mySessions",
 });
-
- connectDB();
-
+connectDB();
 app.use(cors({credentials: true, origin: `${URI}`}));
-// app.set("view engine", "ejs");
-
 app.use(bodyParser.json());
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.urlencoded({extended: true}));
 app.use(
     session({
         secret: "secret",
@@ -53,8 +45,8 @@ app.use(
 //------------------------------------------------------------
 app.post("/login", appController.login_post);
 app.post("/logout", appController.logout_post);
-app.get("/api/auth",appController.ApiAuth_get);//authentication
-app.get('/api/theme', isAuth,dbPg.getQuestionAll);//access to questions
+app.get("/api/auth", appController.ApiAuth_get);//authentication
+app.get('/api/theme', isAuth, dbPg.getQuestionAll);//access to questions
 app.get('/api/themesname', dbPg.getQuestionAllName);//access to questions
 
 //------------------------------------------------------------
